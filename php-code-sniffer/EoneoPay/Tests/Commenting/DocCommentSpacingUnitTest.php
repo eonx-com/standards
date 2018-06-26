b83e01ef4d719f547d54e83c29d658f702ced3b7
NatePage <nathan.page@loyaltycorp.com.au>
2018-06-27 00:17:24 +1000

    Implemented doc comment spacing sniff.

---
+++ php-code-sniffer/EoneoPay/Tests/Commenting/DocCommentSpacingUnitTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\EoneoPay\Standards\CodeSniffer\Commenting;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class DocCommentSpacingUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    protected function getErrorList()
+    {
+        return [
+            10 => 1,
+            11 => 1,
+            12 => 1,
+            14 => 1
+        ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    protected function getWarningList()
+    {
+        return [];
+    }
+}
